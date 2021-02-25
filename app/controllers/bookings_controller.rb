16fa4f197481b2d2c9246d640b5566279160bd49
Zach <zbayard@gmail.com>
2021-02-25 14:19:49 -0500

    bookingpostkinda

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,5 +4,17 @@ class BookingsController < ApplicationController
         @bookings = Booking.all
         render json: @bookings
     end
-    
+
+    def create
+        @booking = Booking.create(booking_params)
+        render json: @booking
+    end
+
+
+    private
+
+    def booking_params
+        params.require(:booking).permit(:user_id, :artist_id, :address, :city, :state, :date, :start_time, :end_time, :payment)
+    end
+
 end
