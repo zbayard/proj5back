79f361400ce5ccb42e18776527b39c84d8cef12f
Zach <zbayard@gmail.com>
2021-03-01 16:33:01 -0500

    fullcrud

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,6 +10,19 @@ class BookingsController < ApplicationController
         render json: @booking
     end
 
+    def update
+        @booking = Booking.find(params[:id])
+        @booking.update(booking_params)
+        render json: @booking
+    end
+
+    def destroy
+        @booking = Booking.find(params[:id])
+        @booking.destroy
+        render json: @booking
+    end
+
+
 
     private
 
