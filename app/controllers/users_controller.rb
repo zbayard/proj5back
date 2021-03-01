b28394a2ca5d4231df131363e45c0a0f758a7e1e
Zach <zbayard@gmail.com>
2021-03-01 11:29:03 -0500

    fullauth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,51 @@
 class UsersController < ApplicationController
+    before_action :authenticate, only: [:show, :update]
+
+    def index
+        @users = User.all
+        render json: @users
+    end
+
+
+    def show
+        render json: @current_user
+    end
+
+
+    def login
+
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            token = JWT.encode({user_id: user.id }  , 'mysecret', 'HS256')
+            render json: { user: UserSerializer.new(user), token: token}
+    
+        else 
+            render json: {errors: ['invalid username or password']}, status: :unauthorized
+        end
+    
+    end
+
+
+    def signup 
+
+        user = User.create(user_params) 
+        
+        if user.valid? 
+            token = JWT.encode({user_id: user.id }, 'mysecret', 'HS256')
+            render json: { user: UserSerializer.new(user), token: token}
+        
+        else 
+            render json: { errors: user.errors.full_messages }, status: :unauthorized
+        end 
+    end
+    
+    
+    private
+
+    def user_params
+        params.permit(:username, :password, :name, :image, :city, :state, :bio)
+    end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   resources :reviews
   resources :artists
   resources :users
+
+
+  post '/login', to: 'users#login'
+  get '/profile', to: 'users#show'
+  patch '/profile', to: 'users#update'
+  post '/signup', to: 'users#signup'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
